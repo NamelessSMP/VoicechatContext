032c187285883cc3af11ee2cbc74285f0c68d46f
TheCatRiX <vahrameev.ilya@yandex.ru>
2023-02-27 16:14:55 +0300

    fix LuckPerms context

--- src/main/java/ru/thecatrix/voicechatcontext/LuckpermsContextPlugin.java
+++ src/main/java/ru/thecatrix/voicechatcontext/LuckpermsContextPlugin.java
@@ -2,11 +2,24 @@ package ru.thecatrix.voicechatcontext;
 
 import net.luckperms.api.context.ContextCalculator;
 import net.luckperms.api.context.ContextConsumer;
+import net.luckperms.api.context.ContextSet;
+import net.luckperms.api.context.ImmutableContextSet;
 import org.bukkit.entity.Player;
 
 public class LuckpermsContextPlugin implements ContextCalculator<Player> {
     @Override
     public void calculate(Player target, ContextConsumer contextConsumer) {
         contextConsumer.accept("voicechat", String.valueOf(VoicechatContext.playerVoicechat.get(target.getUniqueId())));
+    }
+
+    String[] Voicechat = {"true", "false"};
+
+    @Override
+    public ContextSet estimatePotentialContexts() {
+        ImmutableContextSet.Builder builder = ImmutableContextSet.builder();
+        for (String Voicechat : Voicechat) {
+            builder.add("voicechat", Voicechat);
+        }
+        return builder.build();
     }
 }
